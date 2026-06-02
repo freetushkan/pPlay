18f5adbad17957fd7f2ac4aa1217c7d2d37b62f8
freetushkan <freetushkan@gmail.com>
2026-06-02 10:27:11 +0300

    656

--- src/chromecast_service.cpp
+++ src/chromecast_service.cpp
@@ -73,11 +73,15 @@ extern "C" {
         CBS_init(&cbs, bytes, len);
         return RSA_parse_private_key(&cbs);
     }
+    // RSA *RSA_private_key_from_bytes(const uint8_t *bytes, size_t len) {
+    //     const uint8_t *p = bytes;
+    //     return d2i_RSAPrivateKey(nullptr, &p, len);
+    // }
     void EVP_cleanup(void) {}
     void X509V3_EXT_free(void *ext) {}
-    void EVP_MD_CTX_cleanup(EVP_MD_CTX *ctx) { EVP_MD_CTX_reset(ctx); }
-    int X509_set_notBefore(X509 *x, const ASN1_TIME *tm) { return X509_set1_notBefore(x, tm); }
-    int X509_set_notAfter(X509 *x, const ASN1_TIME *tm) { return X509_set1_notAfter(x, tm); }
+    int EVP_MD_CTX_cleanup(EVP_MD_CTX *ctx) { return 1;  }
+    int X509_set_notBefore(X509 *x, const ASN1_TIME *tm) { return X509_set_time(X509_get_notBefore(x), tm) ? 1 : 0; }
+    int X509_set_notAfter(X509 *x, const ASN1_TIME *tm) { return X509_set_time(X509_get_notAfter(x), tm) ? 1 : 0; }
     int EVP_PKEY_assign_RSA(EVP_PKEY *pkey, RSA *key) { return EVP_PKEY_set1_RSA(pkey, key); }
     int EVP_DigestSignUpdate(EVP_MD_CTX *ctx, const void *data, size_t dsize) {
         return EVP_DigestUpdate(ctx, data, dsize);
