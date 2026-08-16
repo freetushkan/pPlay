61fe28ab51fd36847d3615f250e7477c358007a9
freetushkan <freetushkan@gmail.com>
2026-08-16 07:02:33 +0300

    Localtime fix?????

--- src/main.cpp
+++ src/main.cpp
@@ -652,7 +652,7 @@ int main() {
     sceSysmoduleLoadModuleInternal(ORBIS_SYSMODULE_INTERNAL_PAD);
     sceSysmoduleLoadModuleInternal((enum OrbisSysModuleInternal)ORBIS_SYSMODULE_IME_DIALOG);
     // sceNetPoolCreate("pplay", (5 * 1024 * 1024), 0);
-	if (load_sys_modules() != 0) return 0;
+	// if (load_sys_modules() != 0) return 0;
     sceKernelDebugOutText(0, "[pPlay] started\n");
 #endif
 
