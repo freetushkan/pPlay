007d004db04223a46fb3963f0ee5ec90f835fbba
freetushkan <freetushkan@gmail.com>
2026-08-17 02:37:30 +0300

    Fix ime buttons assigment?

--- src/menus/menu_main_options_submenu.cpp
+++ src/menus/menu_main_options_submenu.cpp
@@ -104,6 +104,7 @@ namespace Dialog {
         sceUserServiceGetInitialUser(&uid);
         OrbisImeDialogSetting p; memset(&p, 0, sizeof(p));
         p.enterLabel = ORBIS_BUTTON_LABEL_DEFAULT;
+        p.imeParam.enterKeyType = 1;
 #else
         sceUserServiceGetForegroundUser(&uid);
         SceImeDialogParam p; memset(&p, 0, sizeof(p));
