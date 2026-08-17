9536260bf7ba7a5ed75858c983ea14bacf4f2777
freetushkan <freetushkan@gmail.com>
2026-08-17 03:34:40 +0300

    Fix ime buttons assigment.. Revert.

--- src/menus/menu_main_options_submenu.cpp
+++ src/menus/menu_main_options_submenu.cpp
@@ -104,7 +104,6 @@ namespace Dialog {
         sceUserServiceGetInitialUser(&uid);
         OrbisImeDialogSetting p; memset(&p, 0, sizeof(p));
         p.enterLabel = ORBIS_BUTTON_LABEL_DEFAULT;
-        p.inputMethod = static_cast<OrbisInput>(1);
 #else
         sceUserServiceGetForegroundUser(&uid);
         SceImeDialogParam p; memset(&p, 0, sizeof(p));
